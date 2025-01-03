14ad242054c127fbd5b5aba5959773a1b81c5233
DonizeteVida <donizetevida1998@outlook.com>
2025-01-03 01:13:17 -0300

    Initial Commit

---
+++ MediaUseCases/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediaUseCases
+//
+//  Created by Donizete Vida on 03/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediaUseCases/MediaUseCasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaUseCasesApp.swift
+//  MediaUseCases
+//
+//  Created by Donizete Vida on 03/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MediaUseCasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
