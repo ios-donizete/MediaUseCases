cf6441e478c732bda725cafed1fcc6c067faed7f
DonizeteVida <donizetevida1998@outlook.com>
2025-01-03 02:28:32 -0300

    A simple ViewModel

--- MediaUseCases/ContentView.swift
+++ MediaUseCases/ContentView.swift
@@ -9,31 +9,47 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-    @State var isCameraGranted = false
+    @State var viewModel = ViewModel()
 
     var body: some View {
         VStack {
-            if isCameraGranted {
-                Text("Tap to start recording")
+            if viewModel.isCameraGranted {
+                VStack {
+                    Image(systemName: "camera")
+                    Text("Tap to start recording")
+                }
             } else {
-                Text("Tap to request camera access")
-                    .onTapGesture {
-                        Task.init { await onCameraRequest() }
-                    }
+                VStack {
+                    Label(
+                        "Camera access is required",
+                        systemImage: "exclamationmark.triangle"
+                    ).foregroundStyle(.red)
+                    Spacer().frame(height: 16)
+                    Text("Tap to request camera access")
+                }
+                .padding()
+                .onTapGesture(perform: viewModel.onCameraRequest)
             }
         }
     }
+}
 
-    func onCameraRequest() async {
-        let status = AVCaptureDevice.authorizationStatus(for: .video)
-        
-        var isGranted = status == .authorized
-        
-        if !isGranted {
-            isGranted = await AVCaptureDevice.requestAccess(for: .video)
+extension ContentView {
+
+    @Observable
+    class ViewModel {
+        private(set) var isCameraGranted = false
+
+        init() {
+//            isCameraGranted = AVCaptureDevice
+//                .authorizationStatus(for: .video) == .authorized
         }
 
-        isCameraGranted = isGranted
+        func onCameraRequest() {
+            Task {
+                isCameraGranted =  await AVCaptureDevice.requestAccess(for: .video)
+            }
+        }
     }
 }
 
