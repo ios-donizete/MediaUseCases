25155664255009248c016654e26e69dbf4534817
DonizeteVida <donizetevida1998@outlook.com>
2025-01-03 01:52:27 -0300

    Basic camera permission request code

--- MediaUseCases/ContentView.swift
+++ MediaUseCases/ContentView.swift
@@ -6,16 +6,34 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State var isCameraGranted = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isCameraGranted {
+                Text("Tap to start recording")
+            } else {
+                Text("Tap to request camera access")
+                    .onTapGesture {
+                        Task.init { await onCameraRequest() }
+                    }
+            }
         }
-        .padding()
+    }
+
+    func onCameraRequest() async {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        var isGranted = status == .authorized
+        
+        if !isGranted {
+            isGranted = await AVCaptureDevice.requestAccess(for: .video)
+        }
+
+        isCameraGranted = isGranted
     }
 }
 
